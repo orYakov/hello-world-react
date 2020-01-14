ddab904725f5c079aef14352d0e5b4eaf22bf503
Or Yakov <or15yakov@gmail.com>
2020-01-14 20:45:47 +0200

    ccc

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           bdika1
+          bdika2
         </p>
         <a
           className="App-link"
